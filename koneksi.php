24a4163f6ad5e68915d48eff8b45528265d9b0af
azizah03 <nur341545@gmail.com>
2018-05-30 16:41:50 +0700

    travis

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 class Koneksi {
     private $server = "localhost";
-    private $username = "root"; //root
-    private $password = ""; //""
-    private $db = "php_kelas_c";
+    private $username = "id4927718_dev"; //root
+    private $password = "Alfatih04"; //""
+    private $db = "id4927718_stok_barang";
 
     function getKoneksi() {
         return new mysqli($this->server, $this->username,
